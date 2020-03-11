e480d742078dc29252794e3110d77636c3ac181d
João Silva <Joao.Silva@sinsolution.local>
2020-03-11 11:09:21 -0300

    Ajuste Conflito

---
+++ resources/views/estoque.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/estilo.css"/>
+    <title>Passagem de Turno</title>
+</head>
+
+
+<body>
+
+
+<div class="jumbotron">
+
+
+<h1>Passagem de Turno</h1>
+
+    <nav>
+
+        <ul class="menuLogin">
+
+            <li><a href="#">Cadastro</a></li>
+            <li><a href="#">Login</a></li>
+
+        </ul>
+    </nav>
+
+    <div class="logo">
+        <img src="../interaxanew.png" alt="ITX">
+
+    </div>
+
+</div>
+
+
+<div class="container">
+
+    <h4>Analistas Turno | 22:00 - 06:00</h4><br>
+
+
+<ul>
+    <li>João Alan</li>
+    <li>Lilian</li>
+
+
+</ul>
+
+</div>
+
+    <div class="responsabilidade">
+
+        <div class="container">
+
+    <h4>Responsabilidade Analista </h4><br>
+    <ul>
+
+        <li> Monitoração, CheckList e Telefone: <b>João Alan, Lilian</b></li>
+        <li>Gestão Geral da Fila de Chamados: <b>João Alan, Lilian</b></li>
+        <li>Triagem Inicial da Fila de Chamados: <b>João Alan, Lilian</b></li>
+        <li> Fila De Chamados Em Andamentos e Telefone: <b>João Alan, Lilian</b></li>
+        <li>Transcrição: <b>Lilian</b></li>
+
+    </ul>
+        </div>
+
+</div>
+
+</body>
+</html>
